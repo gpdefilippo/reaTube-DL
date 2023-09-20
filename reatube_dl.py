75a58490d2e5362b3e862af0854905525988ee6a
gpdefilippo <gpdefilippo@gmail.com>
2023-09-19 19:08:30 -0500

    conversion to PyQt5 from Tkinter

--- reatube_dl.py
+++ reatube_dl.py
@@ -5,27 +5,29 @@ Created on Thu Dec  1 22:29:43 2022
 
 @author: ginodefilippo
 """
-from __future__ import unicode_literals
 import os
-import tkinter as tk
-from tkinter import colorchooser
+import sys
+from PyQt5 import QtWidgets, QtGui, QtCore
 import youtube_dl
 import reaper_python as rp
 
-class ReaTubeDl:
-    def __init__(self, root: tk.Tk):
-        self.root = root
+
+class ReaTubeDl(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
         self.download_url = ""
         self.save_dir = '~/Documents/Reaper/ReaTubeDl/'
-        self.color = "#5799db"
+        self.color = QtGui.QColor("#5799db")
 
-        self.setup_tk()
-    
+        self.setup_ui()
+
     def color_choose(self):
-        self.color = colorchooser.askcolor(self.color)
+        color = QtWidgets.QColorDialog.getColor(self.color)
+        if color.isValid():
+            self.color = color
 
-    def on_return(self, event):
-        self.download_url = self.e.get()
+    def download(self):
+        self.download_url = self.url_entry.text()
         out_file = os.path.join(self.save_dir, '%(title)s.%(ext)s')
         ydl_opts = {
             'outtmpl': out_file,
@@ -39,23 +41,34 @@ class ReaTubeDl:
             'progress_hooks': [my_hook],
         }
         download_url(self.download_url, ydl_opts)
-        # --extract-audio, --audio-format "mp3"
-        # -o 'self.save_dir/%(playlist)s/%(chapter_number)s - %(chapter)s/%(title)s.%(ext)s'
-        self.root.destroy()
+        self.close()
 
-    def close(self, event):
-        self.root.quit()
-        self.root.destroy()
- 
-    def setup_tk(self):
-        self.e = tk.Entry(root)
-        self.e.pack()
-        self.b = tk.Button(root, text='Choose Color',command=self.color_choose)
-        self.b.pack(side="left")
-        
-        self.e.bind("<Return>", self.on_return)
-        self.root.bind("<Escape>", self.close)
-        
+    def close(self):
+        self.close()
+
+    def setup_ui(self):
+        self.setWindowTitle("ReaTubeDl")
+
+        self.url_label = QtWidgets.QLabel("Enter URL:")
+        self.url_entry = QtWidgets.QLineEdit()
+        self.color_button = QtWidgets.QPushButton("Choose Color")
+        self.download_button = QtWidgets.QPushButton("Download")
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.url_label)
+        layout.addWidget(self.url_entry)
+        layout.addWidget(self.color_button)
+        layout.addWidget(self.download_button)
+
+        self.setLayout(layout)
+
+        self.color_button.clicked.connect(self.color_choose)
+        self.download_button.clicked.connect(self.download)
+        self.url_entry.returnPressed.connect(self.download)
+
+        self.show()
+
+
 class MyLogger():
     def debug(self, msg):
         pass
@@ -66,15 +79,18 @@ class MyLogger():
     def error(self, msg):
         print(msg)
 
+
 def my_hook(d):
     if d['status'] == 'finished':
         print('Done downloading, now converting ...')
-        
-def download_url(url: str, ydl_opts: dict=None):
+
+
+def download_url(url: str, ydl_opts: dict = None):
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])       
+        ydl.download([url])
 
-root = tk.Tk()
 
-readown = ReaTubeDl(root)
-root.mainloop()
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = ReaTubeDl()
+    sys.exit(app.exec_())
